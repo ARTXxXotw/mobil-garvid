8fefe565a3f341591578d15aa2269d9b1b72e739
Zafar <zufaralievazafarali@gmail.com>
2023-08-16 15:23:58 +0500

    ысыс
    ысысыс

--- worksApp/src/Components/Chat.js
+++ worksApp/src/Components/Chat.js
@@ -13,137 +13,41 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import io from "socket.io-client";
 import socketServices from "../server/socketServices";
-
+const socket = io.connect("https://markazback2.onrender.com");
 const Stack = createStackNavigator();
 
 const ChatScreen = (props) => {
-  const [message, setMessage] = useState("");
-  const [data, setData] = useState([]);
-  const socket = io.connect("https://markazback2.onrender.com");
-  // const [page, setPage] = useState();
-  // const [state1, setState1] = React.useState();
-  // const [rooms, setRooms] = useState([]);
-  // const [room, setRoom] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [users, setUsers] = useState([]);
-  // const [chats, setChats] = useState([]);
-  // const [nameput, Tgnameput] = useState([]);
-  // const url = "https://markazback2.onrender.com";
-  // useEffect(() => {
-  //   const getToken = async () => {
-  //     var token = await AsyncStorage.getItem("token");
-  //     axios
-  //       .get("https://markazback2.onrender.com/auth/oneuser", {
-  //         headers: { Authorization: "Bearer " + token },
-  //       })
-  //       .then((res1) => {
-  //         console.log(res1.data);
-  //         let email = res1.data[0].email;
-  //         socket.emit("authenticate", { email });
-  //         setEmail(email);
-  //         socket.emit("get_rooms", { email });
-  //       })
-  //       .catch((err) => {});
-  //     axios
-  //       .get("https://markazback2.onrender.com/auth/allusers", {
-  //         headers: { Authorization: "Bearer " + token },
-  //       })
-  //       .then((res1) => {
-  //         setUsers(res1.data);
-  //       });
-  //     navigation.getParent().setOptions({ tabBarStyle: { display: "none" } });
-  //     return () => {
-  //       navigation.getParent().setOptions({ tabBarStyle: { display: "flex" } });
-  //     };
-  //   };
-  //   getToken();
-  // }, []);
-  // useEffect(() => {
-  //   socket.on("load_rooms", (data) => {
-  //     setRooms(data);
-  //   });
-  // }, [socket]);
-  // useEffect(() => {
-  //   const getToken = async () => {
-  //     var token = await AsyncStorage.getItem("token");
-  //     axios
-  //       .get(`${url}/auth/oneuser/`, {
-  //         headers: { Authorization: "Bearer " + token },
-  //       })
-  //       .then(async (res) => {
-  //         // res.data.map((item) => {
-  //         //   document.querySelector("#tg_name").value = item.username;
-  //         //   document.querySelector("#tg_number").value = item.phone_number;
-  //         //   document.querySelector("#tg_email").value = item.email;
-  //         // });
-  //         await AsyncStorage.setItem("page_user", JSON.stringify(res.data));
-  //         console.log(res.data);
-  //         setChats(res.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //     socket.on("new_private_room", (data) => {
-  //       setRooms((prevRooms) => [...prevRooms, data.roomName]);
-  //       Swal.fire(`Новая приватная комната создана: ${data.roomName}`);
-  //     });
-  //   };
-  //   getToken()
-  // }, [socket]);
-  // const createPrivateRoom = (otheremail) => {
-  //   // window.location.reload();
-  //   // let email2="piyoz@gmail.com"
-  //   socket.emit("create_private_room", {
-  //     email1: email,
-  //     email2: otheremail,
-  //   });
-  //   socket.emit("get_rooms", { email });
-  //   socket.on("load_rooms", (data) => {
-  //     setRooms(data);
-  //   });
-  //   // getRooms();
-  //   // getUsers();
-  // };
-  // const handleInputChange = async (event) => {
-  //   // setSearch(event.target.value);
-  //   var token = await AsyncStorage.getItem("token");
-
-  //   const searchRegex = new RegExp(`^${event.target.value}`, "i");
-  //   axios
-  //     .get("https://markazback2.onrender.com/auth/allusers", {
-  //       headers: { Authorization: "Bearer " + token },
-  //     })
-  //     .then((res) => {
-  //       const searchdata = res.data.filter((item) => {
-  //         return (
-  //           searchRegex.test(<Text>{item.username}</Text>) ||
-  //           searchRegex.test(<Text>{item.email}</Text>)
-  //         );
-  //       });
-
-  //       setUsers(searchdata);
-  //     });
-  // };
+  const [email, setEmail] = useState("");
+  const [rooms, setRooms] = useState([]);
   useEffect(() => {
-    console.log(socket.on);
-    socket.on("load_rooms", (data) => {});
-    socketServices.initializeSocket();
-  }, [socket]);
-  useEffect(() => {
-    socketServices.on("received_message", (msg) => {
-      console.log("message received in reactNativeApp", msg);
-      let cloneArry = [...data];
-      setData(cloneArry.concat(msg));
+    const getEmail = async () => {
+      const token =  JSON.parse( await AsyncStorage.getItem('token'))
+      // console.log(token);
+      axios
+      .get("https://markazback2.onrender.com/auth/oneuser", {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then((res1) => {
+        // console.log(res1.data,"ssssssssssssss");
+        // Swal.fire(res1.data[0].email)
+        let email = res1.data[0].email;
+        socket.emit("authenticate", { email });
+        setEmail(email);
+        // Swal.fire("ishladi")
+        //     socket.emit("authenticate", { email });
+        //         const getRooms = async () => {
+        socket.emit("get_rooms", { email });
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    };
+    getEmail()
+    socket.on("load_rooms", (data) => {
+      setRooms(data);
+      console.log(data);
     });
-  }, [data]);
-  const sendMessage = () => {
-    if (!!message) {
-      socketServices.emit("send_message", message);
-      setMessage("");
-      return;
-    }
-    alert("please enter your message");
-  };
+  }, []);
   return (
     <ScrollView>
       {/*<View style={{ flexDirection: "row" }}>
@@ -157,206 +61,36 @@ const ChatScreen = (props) => {
           )
         })
       }*/}
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-          }}
-          style={{ width: 70, height: 70, borderRadius: 50 }}
-        />
-        <View style={{ width: "60%" }}>
-          <Text style={{ fontSize: 20 }} numberOfLines={1}>
-            Jane Doe
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-          }}
-          style={{ width: 70, height: 70, borderRadius: 50 }}
-        />
-        <View style={{ width: "60%" }}>
-          <Text style={{ fontSize: 20 }} numberOfLines={1}>
-            Jane Doe
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-          }}
-          style={{ width: 70, height: 70, borderRadius: 50 }}
-        />
-        <View style={{ width: "60%" }}>
-          <Text style={{ fontSize: 20 }} numberOfLines={1}>
-            Jane Doe
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-          }}
-          style={{ width: 70, height: 70, borderRadius: 50 }}
-        />
-        <View style={{ width: "60%" }}>
-          <Text style={{ fontSize: 20 }} numberOfLines={1}>
-            Jane Doe
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-          }}
-          style={{ width: 70, height: 70, borderRadius: 50 }}
-        />
-        <View style={{ width: "60%" }}>
-          <Text style={{ fontSize: 20 }} numberOfLines={1}>
-            Jane Doe
-          </Text>
-        </View>
-      </View>
-      <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: 10,
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-        }}
-        style={{ width: 70, height: 70, borderRadius: 50 }}
-      />
-      <View style={{ width: "60%" }}>
-        <Text style={{ fontSize: 20 }} numberOfLines={1}>
-          Jane Doe
-        </Text>
-      </View>
-    </View>
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: 10,
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-        }}
-        style={{ width: 70, height: 70, borderRadius: 50 }}
-      />
-      <View style={{ width: "60%" }}>
-        <Text style={{ fontSize: 20 }} numberOfLines={1}>
-          Jane Doe
-        </Text>
-      </View>
-    </View>
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: 10,
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-        }}
-        style={{ width: 70, height: 70, borderRadius: 50 }}
-      />
-      <View style={{ width: "60%" }}>
-        <Text style={{ fontSize: 20 }} numberOfLines={1}>
-          Jane Doe
-        </Text>
-      </View>
-    </View>
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: 10,
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-        }}
-        style={{ width: 70, height: 70, borderRadius: 50 }}
-      />
-      <View style={{ width: "60%" }}>
-        <Text style={{ fontSize: 20 }} numberOfLines={1}>
-          Jane Doe
-        </Text>
-      </View>
-    </View>
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: 10,
-      }}
-    >
-      <Image
-        source={{
-          uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
-        }}
-        style={{ width: 70, height: 70, borderRadius: 50 }}
-      />
-      <View style={{ width: "60%" }}>
-        <Text style={{ fontSize: 20 }} numberOfLines={1}>
-          Jane Doe
-        </Text>
-      </View>
-    </View>
+                   {rooms.map((item,key) => {
+                let a = item;
+                if (a !== null) {
+                  const [email1, email2] = a.split("_");
+           
+                  const displayName = email1 === email ? email2 : email1;
+                  return(
+                    <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      padding: 10,
+                    }}
+                  >
+                    <Image
+                      source={{
+                        uri: "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-27.jpg",
+                      }}
+                      style={{ width: 70, height: 70, borderRadius: 50 }}
+                    />
+                    <View style={{ width: "60%" }}>
+                      <Text style={{ fontSize: 20 }} numberOfLines={1}>
+                     {displayName}
+                      </Text>
+                    </View>
+                  </View>
+                  )
+}})}
+
     </ScrollView>
   );
 };
@@ -380,7 +114,7 @@ const UserScreen = ({ navigation }) => {
       >
         <Text>Go Back</Text>
       </TouchableOpacity>
-      <Text>asdasdasd</Text>
+      <Text></Text>
     </View>
   );
 };
